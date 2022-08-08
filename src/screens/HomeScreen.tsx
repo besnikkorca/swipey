4ab05c0c8127be1064f08414b14d12177716ff63
Besnik Korça <besnik@accountable.eu>
2022-08-08 17:03:38 +0200

    feat(swipey): upload pdf ui

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,72 +1,36 @@
 import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  VStack,
-  Box,
-  Button,
-} from "native-base";
-import NativeBaseIcon from "../components/NativeBaseIcon";
+import { Text, HStack, Center, Heading, VStack, Box } from "native-base";
+import CompanyIcon from "../components/CompanyIcon";
 import { NavigationProp } from "@react-navigation/native";
-import { ScreenNames } from "../navigation/types";
-import SessionManager from "../services/SessionManager";
+import Button from "components/Form/Button";
 
 type Props = {
   navigation: NavigationProp<any>;
 };
 
 export default function HomeScreen({ navigation }: Props) {
+  const handleUploadCV = () => {
+    alert("Implement upload CV ");
+  };
   return (
-    <Center
-      _dark={{ bg: "blueGray.900" }}
-      _light={{ bg: "blueGray.50" }}
-      px={4}
-      flex={1}
-    ></Center>
+    <Box flex={1} alignItems="center" mt="5">
+      <Heading size="lg">Welcome to Swipey</Heading>
+      <Box flex={1} justifyContent="center">
+        <CompanyIcon />
+      </Box>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <VStack space={5} alignItems="center">
+          <HStack space={2}>
+            <Text>Upload your CV before you start looking for jobs!</Text>
+          </HStack>
+          <Button onPress={handleUploadCV}>Upload CV</Button>
+        </VStack>
+      </Center>
+    </Box>
   );
-
-  // return (
-  //   <Center
-  //     _dark={{ bg: "blueGray.900" }}
-  //     _light={{ bg: "blueGray.50" }}
-  //     px={4}
-  //     flex={1}
-  //   >
-  //     <VStack space={5} alignItems="center">
-  //       <NativeBaseIcon />
-  //       <Heading size="lg">Welcome to NativeBase</Heading>
-  //       <HStack space={2} alignItems="center">
-  //         <Text>Edit</Text>
-  //         <Box
-  //           _web={{
-  //             _text: {
-  //               fontFamily: "monospace",
-  //               fontSize: "sm",
-  //             },
-  //           }}
-  //           px={2}
-  //           py={1}
-  //           _dark={{ bg: "blueGray.800" }}
-  //           _light={{ bg: "blueGray.200" }}
-  //         >
-  //           App.js
-  //         </Box>
-  //         <Text>and save to reload.</Text>
-  //       </HStack>
-  //       <Link href="https://docs.nativebase.io" isExternal>
-  //         <Text color="primary.500" underline fontSize={"xl"}>
-  //           Learn NativeBase
-  //         </Text>
-  //       </Link>
-  //       <Button onPress={() => navigation.navigate(ScreenNames.about)}>
-  //         Navigate to about
-  //       </Button>
-  //     </VStack>
-  //   </Center>
-  // );
 }
